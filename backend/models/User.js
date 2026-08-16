2e25b357708c3ee1b3d334a1832b5012730ab347
vishal  dhakad <dhakadv894@gmail.com>
2026-08-16 22:38:05 +0530

    Fix formatting in User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,12 +19,16 @@ const userSchema = new mongoose.Schema({
   totalHires: { type: Number, default: 0 },
   createdAt: { type: Date, default: Date.now }
 });
-
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
+
+
+
+
+
 
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
